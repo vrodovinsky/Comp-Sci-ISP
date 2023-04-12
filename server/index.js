699015b10e18f13dbe567175196f7dc59177a08b
Tristan Merkley <tmerkley@bayviewglen.ca>
2023-04-12 09:08:33 -0400

    Basic Get Requests

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const db = require('./db');
-const User = require('./models/User');
+const User = require('./models/user');
+const Service_provider = require('./models/service_provider');
+const Reviews = require('./models/reviews');
+const Appointments = require('./models/appointments');
+const Reviews = require('./models/reviews');
 
 const app = express();
 
@@ -15,6 +19,34 @@ app.get('/api/users', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
+app.get('/api/service_provider', async (req, res) => {
+    try {
+        const service_provider = await Service_provider.find({});
+        res.json(service_provider);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get('/api/reviews', async (req, res) => {
+    try {
+        const reviews = await Reviews.find({});
+        res.json(reviews);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get('/api/appointments', async (req, res) => {
+    try {
+        const appointments = await Appointments.find({});
+        res.json(appointments);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 
 const PORT = process.env.PORT || 3000;
 
