a74e1d26653cf1d1a0fe4a2c2ae9fb59ceb27625
Tristan Merkley <tmerkley@bayviewglen.ca>
2023-05-26 09:32:25 -0400

    Import Formatting

--- TaskTapp-Vue/src/router/index.js
+++ TaskTapp-Vue/src/router/index.js
@@ -8,59 +8,59 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('/src/views/HomeView.vue')
+      component: () => import('../views/HomeView.vue')
     },
     {
       path: '/signup',
       name: 'signup',
-      component: () => import('/src/views/signUp.vue')
+      component: () => import('../views/signUp.vue')
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('/src/views/login.vue')
+      component: () => import('../views/login.vue')
     },
     {
       path: '/search',
       name: 'search',
-      component: () => import('/src/views/search.vue')
+      component: () => import('../views/search.vue')
     },
     {
       path: '/searchMap',
       name: 'searchMap',
-      component: () => import('/src/views/searchMap.vue')
+      component: () => import('../views/searchMap.vue')
     },
     {
       path: '/header',
       name: 'header',
-      component: () => import('/src/components/Header.vue')
+      component: () => import('../components/Header.vue')
     },
     {
       path: "/callback",
       name: "callback",
-      component: () => import("/src/views/callback.vue")
+      component: () => import("../views/callback.vue")
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: () => import('/src/views/Dashboard.vue'),
+      component: () => import('../views/Dashboard.vue'),
       beforeEnter: authGuard,
     },
     {
       path: '/profile',
       name: 'profile',
-      component: () => import('/src/views/profile.vue'),
+      component: () => import('../views/profile.vue'),
       beforeEnter: authGuard,
     },
     {
       path: '/profile',
       name: 'profile',
-      component: () => import('/src/views/profile.vue')
+      component: () => import('../views/profile.vue')
     },
     {
       path: '/Navbar',
       name: 'navbar',
-      component: () => import('/src/components/Navbar.vue')
+      component: () => import('../components/Navbar.vue')
     }
   ]
 })
